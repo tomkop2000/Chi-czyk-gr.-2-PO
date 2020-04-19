9b0d4a21304a16e3db9ea847538ec68ec36c3e57
PanPeople <mikolajek26@gmail.com>
2020-04-20 00:08:07 +0200

    2
    można wybrać pionek, jest proste losowanie oczek oraz można spradzić kolizję z innymi, potrzebna mapa współrzędnych

--- Mikołaj/Gracz.py
+++ Mikołaj/Gracz.py
@@ -1,15 +1,16 @@
-import pygame
+import pygame, sys
 
-class Gracz(object):
+class Gracz():
     kolor = ( (20, 150, 3),(255, 227, 18), (0, 0, 255),(148, 27, 3))  # red green yellow blue
 
     ilosc = 0
 
     pancerz = 1
 
+    def __init__(self, game):
+        self.game = game
 
-
-
+    pozycja=(0,0,0,0)
 
 
     def circle(self,number):
@@ -31,26 +32,74 @@ class Gracz(object):
 
         for i in range(4):
             pygame.draw.circle(self.game.screen, self.kolor[number-1], (int(pozycjaPionkiBaza[i][0]+int(pozycja[0])), int(pozycjaPionkiBaza[i][1]+int(pozycja[1]))), int(self.game.resolution[1]/15/2) )
-            print(pozycja[0],pozycja[1])
 
-    def sprRuch(self):
-        pass
+    def sprRuch(self, a, b ,c ,d,oczka,pionek):
+        for x in range(4):
+            if (a.pozycja[pionek]+oczka)!=b.pozycja[x]:
+                print("nowa pozycja")
+
+            elif (a.pozycja[pionek]+oczka)!=c.pozycja[x]:
+                print("nowa pozycja")
+
+            elif (a.pozycja[pionek]+oczka)!=d.pozycja[x]:
+                print("nowa pozycja")
+
+            else:
+                print("stara pozycja")
 
     def sprWygrana(self):
         pass
 
 class User(Gracz):
 
-    def __init__(self, game):
-        self.game = game
 
     def tick(self):
-        pass
+        self.wyborPionka()
 
     def draw(self,number):
         self.circle(number)
 
     def wyborPionka(self):
-        pionek = input("Jaki pionek:\n")
+
+        pygame.event.clear()
+
+        print("Jaki pionek:\n")
+
+        event = pygame.event.wait()
+
+        pionek = 0
+
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_1:
+            pionek = 1
+            print("Wybrano pionek nr. ", pionek)
+
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_2:
+            pionek = 2
+            print("Wybrano pionek nr. ", pionek)
+
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_3:
+            pionek = 3
+            print("Wybrano pionek nr. ", pionek)
+
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_4:
+            pionek = 4
+            print("Wybrano pionek nr. ", pionek)
+
+        elif event.type == pygame.QUIT:
+            sys.exit(0)
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            sys.exit(0)
+
+        if pionek>=1 and pionek<=4:
+           # print("true")
+            return True
+
+
+        else:
+           # print("false")
+            return False
+
+
+
         print("Wybrano pionek nr. " + pionek)
         return pionek
--- Mikołaj/run.py
+++ Mikołaj/run.py
@@ -1,7 +1,10 @@
 import pygame, sys
 
+import random
+
 from Gracz import User
 
+import os
 
 class Game(object):
 
@@ -21,6 +24,15 @@ class Game(object):
         self.player_3 = User(self)
         self.player_4 = User(self)
 
+
+
+        iterator = 0
+        self.flag = 0
+
+        self.wylosowano = 0
+
+        self.oczka=0
+
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -34,6 +46,18 @@ class Game(object):
                 self.tick()
                 self.tps_delta -= 1 / self.tps_max
 
+            #logika gry
+            # losowanie kostka:
+
+
+
+
+
+                # self.player_1.sprRuch(self.player_1,self.player_2,self.player_3,self.player_4)
+
+
+
+
             #drawing
             self.screen.fill((0,0,0))
             self.draw()
@@ -42,14 +66,41 @@ class Game(object):
 
 
     def tick(self):
-        self.player_1.tick()
 
-        for x in range(4):
-            pass
+        if self.wylosowano == 0:
+            self.oczka = self.losowanie()
+            self.wylosowano = 1
+
+        if   self.player_1.wyborPionka():
+            self.flag = 1
+            self.wylosowano = 0
+            print("to był gracz 1")
+
+
+        elif  self.flag == 1 and self.player_2.wyborPionka():
+            self.flag = 2
+            self.wylosowano = 0
+            print("to był gracz 2")
+
+        elif self.flag == 2 and self.player_3.wyborPionka():
+            self.flag = 3
+            self.wylosowano = 0
+            print("to był gracz 3")
+
+
+        elif  self.flag == 3 and self.player_4.wyborPionka():
+            self.flag = 0
+            self.wylosowano = 0
+            print("to był gracz 4")
+
+
+
+
 
 
     def draw(self):
 
+        #rysowanie planszy
         for x in range(15+1):
             pygame.draw.line(self.screen,(255,255,255),((self.resolution[0]-self.resolution[1])/2,self.resolution[1]/15*(x)),((self.resolution[0]-self.resolution[1])/2+self.resolution[1],self.resolution[1]/15*(x)),5 )
             pygame.draw.line(self.screen, (255, 255, 255), ((self.resolution[0] - self.resolution[1]) / 2 + self.resolution[1]/15*(x),0), ( (self.resolution[0] - self.resolution[1]) / 2 + self.resolution[1]/15*(x),self.resolution[1]), 5)
@@ -58,6 +109,14 @@ class Game(object):
         self.player_2.draw(2)
         self.player_3.draw(3)
         self.player_4.draw(4)
+
+
+
+
+
+    def losowanie(self):
+        oczka =random.randint(1, 6)
+        print("Kostka: ", oczka)
 
 
 if __name__ == "__main__":
