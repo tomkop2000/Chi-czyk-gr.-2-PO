e298b9f2122cbfaf359b3c0159b54c7ce18d1952
PanPeople <mikolajek26@gmail.com>
2020-04-25 15:53:00 +0200

    21

--- Mikołaj/Gracz.py
+++ Mikołaj/Gracz.py
@@ -1,54 +1,151 @@
 import pygame, sys
 
+import random
+
 class Gracz():
-    kolor = ( (20, 150, 3),(255, 227, 18), (0, 0, 255),(148, 27, 3))  # red green yellow blue
+
+    kolor_paleta = ( (20, 150, 3),(255, 227, 18), (0, 0, 255),(148, 27, 3))  # green yellow blue red
+
+    kolor = (0,0,255)
 
     ilosc = 0
 
     pancerz = 1
 
-    def __init__(self, game):
-        self.game = game
+    suma_oczek = 0
 
-    pozycja=(0,0,0,0)
+    #pozycja pionka (absolutna) zależna od mapy_pozycji
+    pozycja=[[0,0],[0,0],[0,0],[0,0]]
 
-
-    def circle(self,number):
-
-        pozycjaPionkiBaza = ((0, 0), (self.game.resolution[1] / 15*2, 0), (0, self.game.resolution[1] / 15*2),(self.game.resolution[1] / 15*2, self.game.resolution[1] / 15*2))  # domyslne pozycje
+    # pozycje pionków (relatywne)
+    pozycjaPionkiBaza = [[0, 0], [48 * 2, 0], [0, 48 * 2], [48 * 2, 48 * 2]]
 
 
-        if number ==1:
-            pozycja= ((self.game.resolution[0]-self.game.resolution[1])/2+self.game.resolution[1]/15*(2),self.game.resolution[1]/15*2)
+    # mapa pozycji (absolutna)
+    mapa_pozycji = [[72,312],[120,312],[168,312],[216,312],[264,312],[312,264],[312,216],[312,168],[312,120],[312,72],
+                    [312,24],[360,24],[408,24],[408,72],[408,120],[408,168],[408,216],[408,264],[456,312],
+                    [504,312],[552,312],[600,312],[648,312],[696,312],[696,360],[696,408],[648,408],[600,408],[552,408],
+                    [504,408],[456,408],[408,456],[408,504],[408,552],[408,600],[408,648],[408,696],[360,696],[312,696],
+                    [312,648],[312,600],[312,552],[312,504],[312,456],[264,408],[216,408],[168,408],[120,408],[72,408],
+                    [24,408],[24,360],[24,312]]
 
-        if number ==2:
-            pozycja= ((self.game.resolution[0]-self.game.resolution[1])/2+self.game.resolution[1]/15*(2)+9*self.game.resolution[1]/15,self.game.resolution[1]/15*2)
+    def __init__(self, screen,numer):
+        self.screen = screen
 
-        if number ==3:
-            pozycja= ((self.game.resolution[0]-self.game.resolution[1])/2+self.game.resolution[1]/15*(2)+9*self.game.resolution[1]/15,self.game.resolution[1]/15*2+9*self.game.resolution[1]/15)
+        if numer== 1:
 
-        if number ==4:
-            pozycja= ((self.game.resolution[0]-self.game.resolution[1])/2+self.game.resolution[1]/15*(2),self.game.resolution[1]/15*2+9*self.game.resolution[1]/15)
+            self.pozycja = [[96 + self.pozycjaPionkiBaza[0][0], 96 + self.pozycjaPionkiBaza[0][1]],
+                            [96 + self.pozycjaPionkiBaza[1][0], 96 + self.pozycjaPionkiBaza[1][1]],
+                            [96 + self.pozycjaPionkiBaza[2][0], 96 + self.pozycjaPionkiBaza[2][1]],
+                            [96 + self.pozycjaPionkiBaza[3][0], 96 + self.pozycjaPionkiBaza[3][1]]]
 
-        for i in range(4):
-            pygame.draw.circle(self.game.screen, self.kolor[number-1], (int(pozycjaPionkiBaza[i][0]+int(pozycja[0])), int(pozycjaPionkiBaza[i][1]+int(pozycja[1]))), int(self.game.resolution[1]/15/2) )
+            self.kolor= self.kolor_paleta[0]
+
+
+        elif numer == 2:
+            self.pozycja = [[528 + self.pozycjaPionkiBaza[0][0], 96 + self.pozycjaPionkiBaza[0][1]],
+                            [528 + self.pozycjaPionkiBaza[1][0], 96 + self.pozycjaPionkiBaza[1][1]],
+                            [528 + self.pozycjaPionkiBaza[2][0], 96 + self.pozycjaPionkiBaza[2][1]],
+                            [528 + self.pozycjaPionkiBaza[3][0], 96 + self.pozycjaPionkiBaza[3][1]]]
+
+            self.kolor = self.kolor_paleta[1]
+
+        elif numer == 3:
+            self.pozycja = [[528 + self.pozycjaPionkiBaza[0][0], 528 + self.pozycjaPionkiBaza[0][1]],
+                            [528 + self.pozycjaPionkiBaza[1][0], 528 + self.pozycjaPionkiBaza[1][1]],
+                            [528 + self.pozycjaPionkiBaza[2][0], 528 + self.pozycjaPionkiBaza[2][1]],
+                            [528 + self.pozycjaPionkiBaza[3][0], 528 + self.pozycjaPionkiBaza[3][1]]]
+
+            self.kolor = self.kolor_paleta[2]
+
+        elif numer == 4:
+            self.pozycja = [[96 + self.pozycjaPionkiBaza[0][0], 528 + self.pozycjaPionkiBaza[0][1]],
+                            [96 + self.pozycjaPionkiBaza[1][0], 528 + self.pozycjaPionkiBaza[1][1]],
+                            [96 + self.pozycjaPionkiBaza[2][0], 528 + self.pozycjaPionkiBaza[2][1]],
+                            [96 + self.pozycjaPionkiBaza[3][0], 528 + self.pozycjaPionkiBaza[3][1]]]
+
+            self.kolor = self.kolor_paleta[3]
+
+        else:
+            pass
+
+        print("Gracz nr.", numer, " uruchomiony.")
+
+
+
+    def circle(self,i):
+
+        pygame.draw.circle(self.screen, self.kolor, (self.pozycja[i][0],self.pozycja[i][1]), 15 )
 
     def sprRuch(self, a, b ,c ,d,oczka,pionek):
         for x in range(4):
-            if (a.pozycja[pionek]+oczka)!=b.pozycja[x]:
+            if (a.pozycja[pionek-1]+oczka)!=b.pozycja[x]:
                 print("nowa pozycja")
+                return 1
 
-            elif (a.pozycja[pionek]+oczka)!=c.pozycja[x]:
+            if (a.pozycja[pionek-1]+oczka)!=c.pozycja[x]:
                 print("nowa pozycja")
+                return 1
 
-            elif (a.pozycja[pionek]+oczka)!=d.pozycja[x]:
+            if (a.pozycja[pionek-1]+oczka)!=d.pozycja[x]:
                 print("nowa pozycja")
+                return 1
 
             else:
                 print("stara pozycja")
+                return 0
 
     def sprWygrana(self):
         pass
+
+    def prostyRuch(self):
+        self.pozycja = [[self.pozycja[0][0]+1,self.pozycja[0][1]+1],[self.pozycja[1][0]+1,self.pozycja[1][1]+1],[self.pozycja[2][0]+1,self.pozycja[2][1]+1],[self.pozycja[3][0]+1,self.pozycja[3][1]+1]]
+
+    def losowyRuchTest(self,numer,pionek,oczka):
+        #pass
+        if self.suma_oczek + oczka > 52:
+            self.suma_oczek=0
+
+        self.suma_oczek+=oczka
+
+
+        self.pozycja[pionek-1] = (self.mapa_pozycji[self.suma_oczek-1][0], self.mapa_pozycji[self.suma_oczek-1][1])
+        print("suma oczek: ",self.suma_oczek)
+
+    def losowanie(self):
+        oczka = random.randint(1, 6)
+        return oczka
+
+    def key_check(self):
+        while True:
+            event = pygame.event.wait()
+            if (event.type == pygame.KEYDOWN and (
+                    event.key == pygame.K_ESCAPE or event.key == pygame.K_1 or event.key == pygame.K_2 or event.key == pygame.K_3 or event.key == pygame.K_4)) or event.type == pygame.QUIT:
+                break
+        if event.type == pygame.QUIT:
+            print("Wyjście")
+            sys.exit(0)
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                print("Wyjście")
+                sys.exit(0)
+            elif event.key == pygame.K_1:
+                print("klawisz_1")
+                return 1
+            elif event.key == pygame.K_2:
+                print("klawisz_2")
+                return 2
+            elif event.key == pygame.K_3:
+                print("klawisz_3")
+                return 3
+            elif event.key == pygame.K_4:
+                print("klawisz_4")
+                return 4
+            else:
+                print("klawisz_inny")
+        else:
+            print("inna akcja")
+
 
 class User(Gracz):
 
@@ -56,50 +153,8 @@ class User(Gracz):
     def tick(self):
         self.wyborPionka()
 
-    def draw(self,number):
-        self.circle(number)
+    def draw(self,i):
+        self.circle(i)
 
     def wyborPionka(self):
-
-        pygame.event.clear()
-
-        print("Jaki pionek:\n")
-
-        event = pygame.event.wait()
-
-        pionek = 0
-
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_1:
-            pionek = 1
-            print("Wybrano pionek nr. ", pionek)
-
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_2:
-            pionek = 2
-            print("Wybrano pionek nr. ", pionek)
-
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_3:
-            pionek = 3
-            print("Wybrano pionek nr. ", pionek)
-
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_4:
-            pionek = 4
-            print("Wybrano pionek nr. ", pionek)
-
-        elif event.type == pygame.QUIT:
-            sys.exit(0)
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            sys.exit(0)
-
-        if pionek>=1 and pionek<=4:
-           # print("true")
-            return True
-
-
-        else:
-           # print("false")
-            return False
-
-
-
-        print("Wybrano pionek nr. " + pionek)
-        return pionek
+        pass
--- Mikołaj/run.py
+++ Mikołaj/run.py
@@ -1,123 +1,269 @@
+# coding=utf-8
 import pygame, sys
-
+import pygame.locals
 import random
-
 from Gracz import User
 
-import os
-
-class Game(object):
-
-    def __init__(self):
-        #config
-        self.tps_max = 60
-        self.resolution = (1280 , 720)
-
-        #initialization
-        pygame.init()
-        self.screen = pygame.display.set_mode((self.resolution[0],self.resolution[1]))
-        self.tps_clock = pygame.time.Clock()
-        self.tps_delta = 0.0
-
-        self.player_1 = User(self)
-        self.player_2 = User(self)
-        self.player_3 = User(self)
-        self.player_4 = User(self)
+pygame.init()  # funkcja ładująca moduły pyGame'a odpowiedzialne m.in. za dźwięk czy grafikę
 
 
+class Board(object):
+    """
+    Plansza do gry. Odpowiada za rysowanie okna gry.
+    """
 
-        iterator = 0
-        self.flag = 0
+    def __init__(width, height):
+        """
+        Konstruktor planszy do gry. Przygotowuje okienko gry.
+        """
 
-        self.wylosowano = 0
 
-        self.oczka=0
+resolution_x = 720
+resolution_y = 720
 
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit(0)
-                elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                    sys.exit(0)
+okno = pygame.display.set_mode((resolution_x, resolution_y))
 
-            #ticking
-            self.tps_delta += self.tps_clock.tick()/1000.0
-            while self.tps_delta > 1 / self.tps_max:
-                self.tick()
-                self.tps_delta -= 1 / self.tps_max
+pygame.display.set_caption('Chińczyk')
 
-            #logika gry
-            # losowanie kostka:
+#numery od 1 do 4 to numer gracza
+player_1 = User(okno,1)
+player_2 = User(okno,2)
+player_3 = User(okno,3)
+player_4 = User(okno,4)
 
 
 
 
 
-                # self.player_1.sprRuch(self.player_1,self.player_2,self.player_3,self.player_4)
+def board_draw():
+    pygame.draw.rect(okno, (0, 0, 0), (0, 0, 225, 224))
+    pygame.draw.rect(okno, (0, 150, 0), (1, 1, 224, 223))
+    pygame.draw.rect(okno, (0, 0, 0), (49, 49, 123, 123))
+    pygame.draw.rect(okno, (255, 255, 255), (50, 50, 121, 121))
+
+    pygame.draw.ellipse(okno, (0, 150, 0), (60, 60, 45, 45))
+    pygame.draw.ellipse(okno, (0, 150, 0), (115, 60, 45, 45))
+    pygame.draw.ellipse(okno, (0, 150, 0), (60, 115, 45, 45))
+    pygame.draw.ellipse(okno, (0, 150, 0), (115, 115, 45, 45))
+
+    pygame.draw.rect(okno, (0, 0, 0), (225, 0, 150, 225))
+
+    pygame.draw.rect(okno, (0, 0, 0), (375, 0, 225, 224))
+    pygame.draw.rect(okno, (255, 0, 0), (376, 1, 224, 223))
+    pygame.draw.rect(okno, (0, 0, 0), (430, 49, 123, 123))
+    pygame.draw.rect(okno, (255, 255, 255), (431, 50, 121, 121))
+
+    pygame.draw.ellipse(okno, (255, 0, 0), (435, 60, 45, 45))
+    pygame.draw.ellipse(okno, (255, 0, 0), (490, 60, 45, 45))
+    pygame.draw.ellipse(okno, (255, 0, 0), (435, 115, 45, 45))
+    pygame.draw.ellipse(okno, (255, 0, 0), (490, 115, 45, 45))
+
+    pygame.draw.rect(okno, (255, 255, 255), (226, 1, 49, 36))
+    pygame.draw.rect(okno, (255, 255, 255), (276, 1, 49, 36))  # 1
+    pygame.draw.rect(okno, (255, 255, 255), (326, 1, 49, 36))
+    pygame.draw.rect(okno, (255, 255, 255), (226, 38, 49, 36))
+    pygame.draw.rect(okno, (255, 0, 0), (276, 38, 49, 36))  # 2
+    pygame.draw.rect(okno, (255, 0, 0), (326, 38, 49, 36))
+    pygame.draw.rect(okno, (255, 255, 255), (226, 75, 49, 36))
+    pygame.draw.rect(okno, (255, 0, 0), (276, 75, 49, 36))  # 3
+    pygame.draw.rect(okno, (255, 255, 255), (326, 75, 49, 36))
+    pygame.draw.rect(okno, (255, 255, 255), (226, 112, 49, 36))
+    pygame.draw.rect(okno, (255, 0, 0), (276, 112, 49, 36))  # 4
+    pygame.draw.rect(okno, (255, 255, 255), (326, 112, 49, 36))
+    pygame.draw.rect(okno, (255, 255, 255), (226, 149, 49, 36))
+    pygame.draw.rect(okno, (255, 0, 0), (276, 149, 49, 36))  # 5
+    pygame.draw.rect(okno, (255, 255, 255), (326, 149, 49, 36))
+    pygame.draw.rect(okno, (255, 255, 255), (226, 186, 49, 37))
+    pygame.draw.rect(okno, (255, 0, 0), (276, 186, 49, 37))  # 6
+    pygame.draw.rect(okno, (255, 255, 255), (326, 186, 49, 37))
+
+    pygame.draw.rect(okno, (0, 0, 0), (0, 225, 225, 150))
+
+    pygame.draw.rect(okno, (255, 255, 255), (1, 225, 36, 49))  ##########
+    pygame.draw.rect(okno, (0, 150, 0), (38, 225, 36, 49))
+    pygame.draw.rect(okno, (255, 255, 255), (75, 225, 36, 49))
+    pygame.draw.rect(okno, (255, 255, 255), (112, 225, 36, 49))  # lewa
+    pygame.draw.rect(okno, (255, 255, 255), (149, 225, 37, 49))
+    pygame.draw.rect(okno, (255, 255, 255), (187, 225, 37, 49))  ##########  1
+    pygame.draw.rect(okno, (255, 255, 255), (376, 225, 37, 49))
+    pygame.draw.rect(okno, (255, 255, 255), (414, 225, 37, 49))
+    pygame.draw.rect(okno, (255, 255, 255), (452, 225, 36, 49))  # prawa
+    pygame.draw.rect(okno, (255, 255, 255), (489, 225, 36, 49))
+    pygame.draw.rect(okno, (255, 255, 255), (526, 225, 36, 49))
+    pygame.draw.rect(okno, (255, 255, 255), (563, 225, 36, 49))  ##########
+
+    pygame.draw.rect(okno, (255, 255, 255), (1, 275, 36, 49))  ##########
+    pygame.draw.rect(okno, (0, 150, 0), (38, 275, 36, 49))
+    pygame.draw.rect(okno, (0, 150, 0), (75, 275, 36, 49))
+    pygame.draw.rect(okno, (0, 150, 0), (112, 275, 36, 49))  # lewa
+    pygame.draw.rect(okno, (0, 150, 0), (149, 275, 37, 49))
+    pygame.draw.rect(okno, (0, 150, 0), (187, 275, 37, 49))  ##########  2
+    pygame.draw.rect(okno, (0, 0, 255), (376, 275, 37, 49))
+    pygame.draw.rect(okno, (0, 0, 255), (414, 275, 37, 49))
+    pygame.draw.rect(okno, (0, 0, 255), (452, 275, 36, 49))  # prawa
+    pygame.draw.rect(okno, (0, 0, 255), (489, 275, 36, 49))
+    pygame.draw.rect(okno, (0, 0, 255), (526, 275, 36, 49))
+    pygame.draw.rect(okno, (255, 255, 255), (563, 275, 36, 49))  ##########
+
+    pygame.draw.rect(okno, (255, 255, 255), (1, 325, 36, 49))  ##########
+    pygame.draw.rect(okno, (255, 255, 255), (38, 325, 36, 49))
+    pygame.draw.rect(okno, (255, 255, 255), (75, 325, 36, 49))
+    pygame.draw.rect(okno, (255, 255, 255), (112, 325, 36, 49))  # lewa
+    pygame.draw.rect(okno, (255, 255, 255), (149, 325, 37, 49))
+    pygame.draw.rect(okno, (255, 255, 255), (187, 325, 37, 49))  ##########  3
+    pygame.draw.rect(okno, (255, 255, 255), (376, 325, 37, 49))
+    pygame.draw.rect(okno, (255, 255, 255), (414, 325, 37, 49))
+    pygame.draw.rect(okno, (255, 255, 255), (452, 325, 36, 49))  # prawa
+    pygame.draw.rect(okno, (255, 255, 255), (489, 325, 36, 49))
+    pygame.draw.rect(okno, (0, 0, 255), (526, 325, 36, 49))
+    pygame.draw.rect(okno, (255, 255, 255), (563, 325, 36, 49))  ##########
+
+    pygame.draw.rect(okno, (0, 0, 0), (0, 373, 225, 224))
+    pygame.draw.rect(okno, (120, 0, 255), (1, 374, 224, 223))
+    pygame.draw.rect(okno, (0, 0, 0), (49, 425, 123, 123))
+    pygame.draw.rect(okno, (255, 255, 255), (50, 426, 121, 121))
+
+    pygame.draw.ellipse(okno, (120, 0, 255), (60, 433, 45, 45))
+    pygame.draw.ellipse(okno, (120, 0, 255), (115, 433, 45, 45))
+    pygame.draw.ellipse(okno, (120, 0, 255), (60, 483, 45, 45))
+    pygame.draw.ellipse(okno, (120, 0, 255), (115, 483, 45, 45))
+
+    pygame.draw.rect(okno, (0, 0, 0), (375, 373, 225, 224))
+    pygame.draw.rect(okno, (0, 0, 255), (376, 374, 224, 223))
+    pygame.draw.rect(okno, (0, 0, 0), (430, 431, 123, 123))
+    pygame.draw.rect(okno, (255, 255, 255), (431, 431, 121, 121))
+
+    pygame.draw.ellipse(okno, (0, 0, 255), (445, 440, 45, 45))
+    pygame.draw.ellipse(okno, (0, 0, 255), (495, 440, 45, 45))
+    pygame.draw.ellipse(okno, (0, 0, 255), (445, 495, 45, 45))
+    pygame.draw.ellipse(okno, (0, 0, 255), (495, 495, 45, 45))
+
+    pygame.draw.rect(okno, (255, 255, 255), (226, 375, 49, 37))
+    pygame.draw.rect(okno, (120, 0, 255), (276, 375, 49, 37))  # 1
+    pygame.draw.rect(okno, (255, 255, 255), (326, 375, 49, 37))
+    pygame.draw.rect(okno, (255, 255, 255), (226, 413, 49, 37))
+    pygame.draw.rect(okno, (120, 0, 255), (276, 413, 49, 37))  # 2
+    pygame.draw.rect(okno, (255, 255, 255), (326, 413, 49, 37))
+    pygame.draw.rect(okno, (255, 255, 255), (226, 451, 49, 36))
+    pygame.draw.rect(okno, (120, 0, 255), (276, 451, 49, 36))  # 3
+    pygame.draw.rect(okno, (255, 255, 255), (326, 451, 49, 36))
+    pygame.draw.rect(okno, (255, 255, 255), (226, 488, 49, 36))
+    pygame.draw.rect(okno, (120, 0, 255), (276, 488, 49, 36))  # 4
+    pygame.draw.rect(okno, (255, 255, 255), (326, 488, 49, 36))
+    pygame.draw.rect(okno, (120, 0, 255), (226, 525, 49, 36))
+    pygame.draw.rect(okno, (120, 0, 255), (276, 525, 49, 36))  # 5
+    pygame.draw.rect(okno, (255, 255, 255), (326, 525, 49, 36))
+    pygame.draw.rect(okno, (255, 255, 255), (226, 562, 49, 36))
+    pygame.draw.rect(okno, (255, 255, 255), (276, 562, 49, 36))  # 6
+    pygame.draw.rect(okno, (255, 255, 255), (326, 562, 49, 36))
+
+
+
+    # print("Plansza narysowana")
+
+    for x in range(15 + 1):
+        pygame.draw.line(okno, (89, 89, 89),(48*x, 0),(48*x,720), 5)
+        pygame.draw.line(okno,  (89, 89, 89),(0, 48*x),(720,48*x), 5)
 
 
 
 
-            #drawing
-            self.screen.fill((0,0,0))
-            self.draw()
-            pygame.display.flip()
+    return 1
+
+
+def losowanie():
+    oczka =random.randint(1, 6)
+    print("Kostka: ", oczka)
+    return oczka
+
+def key_check():
+    while True:
+        event = pygame.event.wait()
+        if (event.type == pygame.KEYDOWN and (event.key == pygame.K_ESCAPE or event.key == pygame.K_1 or event.key == pygame.K_2 or event.key == pygame.K_3 or event.key == pygame.K_4)) or event.type == pygame.QUIT:
+            break
+    if event.type == pygame.QUIT:
+        print("Wyjście")
+        sys.exit(0)
+    elif event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_ESCAPE:
+            print("Wyjście")
+            sys.exit(0)
+        elif event.key == pygame.K_1:
+            print("klawisz_1")
+            return 1
+        elif event.key == pygame.K_2:
+            print("klawisz_2")
+            return 2
+        elif event.key == pygame.K_3:
+            print("klawisz_3")
+            return 3
+        elif event.key == pygame.K_4:
+            print("klawisz_4")
+            return 4
+        else:
+            print("klawisz_inny")
+    else:
+        print("inna akcja")
+
+
+event = pygame.event.clear()
+
+counter = 0
+
+
+board_draw()
+for i in range(4):
+    player_1.draw(i)
+    player_2.draw(i)
+    player_3.draw(i)
+    player_4.draw(i)
+
+pygame.display.update()
+1
+
+while True:
+
+
+    #pętla gry nie urzywamy systemu tick i delta time bo klatka gry jest zamrażana
+    counter += board_draw()
 
 
 
-    def tick(self):
-
-        if self.wylosowano == 0:
-            self.oczka = self.losowanie()
-            self.wylosowano = 1
-
-        if   self.player_1.wyborPionka():
-            self.flag = 1
-            self.wylosowano = 0
-            print("to był gracz 1")
+    print("Plansza nr. ", counter, " narysowana")
 
 
-        elif  self.flag == 1 and self.player_2.wyborPionka():
-            self.flag = 2
-            self.wylosowano = 0
-            print("to był gracz 2")
+    oczka = player_1.losowanie()
 
-        elif self.flag == 2 and self.player_3.wyborPionka():
-            self.flag = 3
-            self.wylosowano = 0
-            print("to był gracz 3")
+    oczka = player_2.losowanie()
 
+    oczka = player_3.losowanie()
 
-        elif  self.flag == 3 and self.player_4.wyborPionka():
-            self.flag = 0
-            self.wylosowano = 0
-            print("to był gracz 4")
+    oczka = player_4.losowanie()
+
+    #player_1.prostyRuch()
+
+    player_1.losowyRuchTest(1,1,1)
+    print(player_1.pozycja)
 
 
 
 
 
 
-    def draw(self):
-
-        #rysowanie planszy
-        for x in range(15+1):
-            pygame.draw.line(self.screen,(255,255,255),((self.resolution[0]-self.resolution[1])/2,self.resolution[1]/15*(x)),((self.resolution[0]-self.resolution[1])/2+self.resolution[1],self.resolution[1]/15*(x)),5 )
-            pygame.draw.line(self.screen, (255, 255, 255), ((self.resolution[0] - self.resolution[1]) / 2 + self.resolution[1]/15*(x),0), ( (self.resolution[0] - self.resolution[1]) / 2 + self.resolution[1]/15*(x),self.resolution[1]), 5)
-
-        self.player_1.draw(1)
-        self.player_2.draw(2)
-        self.player_3.draw(3)
-        self.player_4.draw(4)
 
 
 
 
 
-    def losowanie(self):
-        oczka =random.randint(1, 6)
-        print("Kostka: ", oczka)
 
 
-if __name__ == "__main__":
-    Game()
+    for i in range(4):
+        player_1.draw(i)
+        player_2.draw(i)
+        player_3.draw(i)
+        player_4.draw(i)
+
+    pygame.display.update()
+
+
+    print(key_check())
